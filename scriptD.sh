6519bb56c905c4417be2c079b1ed40c3b0d85617
Cris Vicuna <cristina151313@gmail.com>
2022-10-16 14:53:34 -0500

    Primer commit

---
+++ scriptD.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+: '
+Escribid un script que copie un archivo sobre otro, garantizando que solo reciba dos parámetros.
+'
+
+# Set arguments into a variable
+args=("$@")
+
+# Get the arguments numbers
+param=${#args[@]}
+if [ "$param" -lt 3 ]; then
+    if [ -f "$1" ] || [ -e "$2" ]; then
+        # Overwrite file
+		cat "$1" > "$2"
+        echo "$1 overwrite to $2 successfully"
+    fi
+else
+    echo "Only two input parameters are allowed"
+fi
+
+exit 0
+
+
+
